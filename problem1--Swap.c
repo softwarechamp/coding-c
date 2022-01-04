808becb4fef5ee8df8fd5867b6db3e592f4ebde4
softwarechamp <59526146+softwarechamp@users.noreply.github.com>
2022-01-04 21:16:23 +0530

    Update problem1--Swap.c

--- problem1--Swap.c
+++ problem1--Swap.c
@@ -2,7 +2,7 @@
 main()
 {  
   float num1,num2,num1_2,num2_1;
-printf("enter 2 numbers in the following format\n--> first number,second number");
+printf("enter 2 numbers in the following format\n--> first number,second number\n");
 scanf("%f,%f",&num1,&num2);
 
 num1_2=num2;
